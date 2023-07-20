1be76f8d36387022a0f2c59757cbf55e87255844
Ange0608 <angelique.dartevelle@hotmail.fr>
2023-07-20 11:19:43 +0200

    make model

---
+++ app/Models/Collection.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Collection extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'name',
+        'types',
+        'content',
+        'image',
+        'number_players',
+        'duration',
+        'age',
+        'origin',
+        'example',
+        'description',
+        'status',
+    ];
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'collections_tags', 'collection_id', 'tag_id');
+    }
+
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany(Article::class, 'collections_articles', 'article_id', 'collection_id');
+    }
+
+}
