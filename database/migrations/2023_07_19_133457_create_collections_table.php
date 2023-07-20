fa512915c29dc0232d040fbe637c97b766c2e04e
Ange0608 <angelique.dartevelle@hotmail.fr>
2023-07-20 10:07:51 +0200

    create table collection, and pivot table

---
+++ database/migrations/2023_07_19_133457_create_collections_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collections', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->enum('types',['jeu','concept','art']);
+            $table->longText('content');
+            $table->string('image')->nullable();
+            $table->integer('number_players')->nullable();
+            $table->integer('duration')->nullable();
+            $table->integer('age')->nullable();
+            $table->string('origin')->nullable();
+            $table->longText('example')->nullable();
+            $table->longText('description')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collections');
+    }
+};
--- database/migrations/2023_07_19_140651_create_collections_articles_table.php
+++ database/migrations/2023_07_19_140651_create_collections_articles_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('collections', function (Blueprint $table) {
+        Schema::create('collections_articles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('article_id');
+            $table->foreignId('collection_id');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('collections');
+        Schema::dropIfExists('collections_articles');
     }
 };
