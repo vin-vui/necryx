58b57cb8938125a22f5b9b3bc5ad376fda0d550c
Mia-GH <127387162+Mia-gh@users.noreply.github.com>
2023-07-21 15:29:34 +0200

    tag-controller-maj

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Collection;
-use App\Models\Tage;
+use App\Models\Tag;
 use App\Models\Article;
 use Illuminate\Support\Facades\Storage;
 
