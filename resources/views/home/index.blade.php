92e8762da7c9f38100c5daf7c6fde37adbd08555
Ange0608 <angelique.dartevelle@hotmail.fr>
2023-08-25 08:56:59 +0200

    add photo profil

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -231,7 +231,7 @@
             <div class="-mt-8 w-full max-w-2xl xl:-mb-8 xl:w-96 xl:flex-none">
                 <div class="relative aspect-[2/1] h-full md:-mx-8 xl:mx-0 xl:aspect-auto">
                     <img class="absolute inset-0 h-full w-full rounded-2xl bg-gray-800 object-cover shadow-2xl"
-                        src="/../Photo_Profil.jpg"
+                        src="{{ Storage::url('Photo_Profil.jpg') }}"
                         alt="">
                 </div>
             </div>
