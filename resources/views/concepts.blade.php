4003419d5da7c8d47bcf5fa8b263c245e75a049f
Nara64 <127386715+Nara64@users.noreply.github.com>
2023-08-25 08:51:42 +0200

    button color

--- resources/views/concepts.blade.php
+++ resources/views/concepts.blade.php
@@ -128,7 +128,7 @@
                 </div>
               </div>
               <div class="mt-8 flex justify-end">
-                <button type="submit" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Envoyer</button>
+                <button type="submit" class="rounded-md bg-dark-red px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Envoyer</button>
               </div>
             </div>
           </form>
