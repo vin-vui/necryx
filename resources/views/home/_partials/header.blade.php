e1e8e57267d99cc536f07784aae094bcb10468fd
vinvui <admin@vinvui.com>
2023-08-30 13:31:56 +0200

    Hotfix #1 Add Dashboard button

--- resources/views/home/_partials/header.blade.php
+++ resources/views/home/_partials/header.blade.php
@@ -79,7 +79,9 @@
         </a>
         <div class="flex flex-1 justify-end">
             @auth
-
+            <a href="{{ route('dashboard') }}" class="bg-yellow-400 hover:bg-yellow-500 transition-all text-xs uppercase tracking-widest inline-flex items-center gap-2 py-3 px-4">
+                Dashboard
+            </a>
             @else
             <a href="{{ route('login') }}" class="py-2 px-4 text-sm font-semibold leading-6 font-p text-white">Se connecter</a>
             <a href="{{ route('register') }}" class="bg-yellow-400 hover:bg-yellow-500 transition-all text-xs uppercase tracking-widest inline-flex items-center gap-2 py-3 px-4">
