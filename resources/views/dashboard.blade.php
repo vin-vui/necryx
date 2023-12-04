0e5750c69ffffa86270c3fe406a523f1aca892da
vinvui <admin@vinvui.com>
2023-12-04 09:14:32 +0100

    Hotfix #1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                
+                <iframe plausible-embed src="https://plausible.io/share/necryx.com?auth=Xsz6N3YlMnHkkUkmG2ja2&embed=true&theme=dark" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
+                <div style="font-size: 14px; padding-bottom: 14px;">Stats powered by <a target="_blank" style="color: #4F46E5; text-decoration: underline;" href="https://plausible.io">Plausible Analytics</a></div>
+                <script async src="https://plausible.io/js/embed.host.js"></script>
             </div>
         </div>
     </div>
