aa7f1100cab1672c5f307925198b08fca784eff8
vinvui <admin@vinvui.com>
2023-12-04 09:33:12 +0100

    Add dashboard with Plausible

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,39 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="max-w-none mx-auto sm:px-6 lg:px-8">
+            <div class="bg-gray-900">
+                <div class="mx-auto ">
+                    <div class="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 lg:grid-cols-4">
+                        <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
+                            <p class="text-sm font-medium leading-6 text-gray-400">Article publiés</p>
+                            <p class="mt-2 flex items-baseline gap-x-2">
+                                <span class="text-4xl font-semibold tracking-tight text-white">{{ App\Models\Article::where('status', 'published')->count() }}</span>                            </p>
+                        </div>
+                        <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
+                            <p class="text-sm font-medium leading-6 text-gray-400">Jeux</p>
+                            <p class="mt-2 flex items-baseline gap-x-2">
+                                <span class="text-4xl font-semibold tracking-tight text-white">{{ App\Models\Collection::where('type_id', 2)->where('status', 'published')->count() }}</span>
+                            </p>
+                        </div>
+                        <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
+                            <p class="text-sm font-medium leading-6 text-gray-400">Arts</p>
+                            <p class="mt-2 flex items-baseline gap-x-2">
+                                <span class="text-4xl font-semibold tracking-tight text-white">{{ App\Models\Collection::where('type_id', 1)->where('status', 'published')->count() }}</span>
+                            </p>
+                        </div>
+                        <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
+                            <p class="text-sm font-medium leading-6 text-gray-400">Concepts</p>
+                            <p class="mt-2 flex items-baseline gap-x-2">
+                                <span class="text-4xl font-semibold tracking-tight text-white">{{ App\Models\Collection::where('type_id', 3)->where('status', 'published')->count() }}</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-12">
                 <iframe plausible-embed src="https://plausible.io/share/necryx.com?auth=Xsz6N3YlMnHkkUkmG2ja2&embed=true&theme=dark" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
-                <div style="font-size: 14px; padding-bottom: 14px;">Stats powered by <a target="_blank" style="color: #4F46E5; text-decoration: underline;" href="https://plausible.io">Plausible Analytics</a></div>
                 <script async src="https://plausible.io/js/embed.host.js"></script>
             </div>
         </div>
