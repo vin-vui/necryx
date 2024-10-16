0f68619693630a0d0e3f00b4e90b2d1e8d4bdd0b
vinvui <vincent.vuillemin@gmail.com>
2024-10-16 10:53:06 +0200

    Hotfix #1

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -39,7 +39,7 @@
         'content' => 'Explorez notre collection de jeux et plongez dans des univers uniques. Découvrez des mécanismes de jeu innovants qui raviront les joueurs de tous horizons !',
         'icon' => '<path fill="#0a0101" d="M20.5 11H19V7a2 2 0 0 0-2-2h-4V3.5A2.5 2.5 0 0 0 10.5 1A2.5 2.5 0 0 0 8 3.5V5H4a2 2 0 0 0-2 2v3.8h1.5c1.5 0 2.7 1.2 2.7 2.7c0 1.5-1.2 2.7-2.7 2.7H2V20a2 2 0 0 0 2 2h3.8v-1.5c0-1.5 1.2-2.7 2.7-2.7c1.5 0 2.7 1.2 2.7 2.7V22H17a2 2 0 0 0 2-2v-4h1.5a2.5 2.5 0 0 0 2.5-2.5a2.5 2.5 0 0 0-2.5-2.5Z" />',
         'textButton' => 'visitez notre section jeux',
-        'link' => route('collections', 2)
+        'link' => route('collections', 5)
         ])
 
         <!--Third card-->
